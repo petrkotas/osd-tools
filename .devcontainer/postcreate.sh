46a57a88cd8e53ec130ca3ba0e2e682a17b9836c
Petr <pkotas@redhat.com>
2024-09-04 15:12:47 +0200

    initial commit

---
+++ .devcontainer/postcreate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Configure this to fit your username and email
+# This is done by mounting the user .gitconfig file into the container
+# Command can be found in the devcontainer.json file almost at the end
+# "mounts": [ ...
+
+# This sets the git editor to the VS Code editor
+git config --global core.editor "code --wait"
+
+# Source git completions and add them to bashrc
+source /usr/share/bash-completion/completions/git
+echo "source /usr/share/bash-completion/completions/git" >> ~/.bashrc
+
+go install github.com/openshift/backplane-tools@latest; backplane-tools install all
+echo "export PATH=${HOME}/.local/bin/backplane/latest:${PATH}" >> ~/.bashrc
+
+# Setup the welcome screen
+echo "cat .devcontainer/motd" >> ~/.bashrc
+echo "cat .devcontainer/motd" >> ~/.zshrc
---
+++ readme.md
@@ -0,0 +1,1 @@
+# OSD tooling in one place
